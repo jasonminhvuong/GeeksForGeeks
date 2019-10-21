4f6e29a3c951bfd95632d26bf8f618e1d3fe9357
Jason Vuong <jasonminhvuong@gmail.com>
2019-10-20 21:30:44 -0600

    Finish Print Alternate Elements of an Array Problem

---
+++ Arrays/print_alternate_elements_of_an_array.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+# arr is the array
+# n is the number of elements in array
+def printAl(arr,n):
+    for i in range(n):
+        if i % 2 == 0:
+            print(arr[i], end=" ")
