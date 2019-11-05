8b4a77e6834774b78a2310d143f0d71f539efb31
Jason Vuong <jasonminhvuong@gmail.com>
2019-11-05 16:25:23 -0700

    Finish Preorder Traversal Problem

---
+++ Tree/preorder_traversal.py
@@ -0,0 +1,33 @@
+"""
+    User Task:
+    Since this is a functional problem you don't have to worry about input,
+    you just have to complete the function preorder() that prints the data in
+    preorder way. The newline is automatically appended by the driver code.
+"""
+
+# Node Class:
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+
+def preorder(root):
+    '''
+    :param root: root of the given tree.
+    :return: None, print the space separated pre order Traversal of the given tree.
+    {
+        # Node Class:
+            class Node:
+                def __init__(self,val):
+                    self.data = val
+                    self.left = None
+                    self.right = None
+    }
+    '''
+    if not root:
+        return
+    
+    print(root.data, end=" ")
+    preorder(root.left)
+    preorder(root.right
